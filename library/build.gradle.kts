6043194f52598781f400821dd82de592be5fadbd
jordanrojasalarcon <rojas.curl@gmail.com>
2023-09-22 20:49:59 -0500

    feat(jitpack): add config

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,8 +1,30 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    //id("com.android.library")
+    id("maven-publish")
 }
 
 
 dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["kotlin"])
+
+                groupId = "com.github.nowjordanhappy"
+                artifactId = "alltoovalidator"
+                version = "0.1.1"
+            }
+        }
+
+        repositories {
+            maven {
+                url = uri("https://jitpack.io")
+            }
+        }
+    }
 }
